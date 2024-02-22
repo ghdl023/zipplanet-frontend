12cc02170eef4e81bf82dafd37fd162c1f752a1f
Usopp <gwee1216@naver.com>
2024-02-22 10:27:26 +0900

    배포 base url 수정

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/zipplanet-frontend/", // https://<USERNAME>.github.io/<REPO>/ 형태로 배포하기위해
 })
