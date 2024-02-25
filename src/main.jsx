50a50ff6b157ade90f17b9c0da4dad86edc59b85
ghdl023 <gwee1216@naver.com>
2024-02-26 00:48:38 +0900

    baseurl 환경변수로 설정

--- src/main.jsx
+++ src/main.jsx
@@ -7,26 +7,31 @@ import Register from './pages/Register';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import '@styles/reset.css';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      // loader: appLoader,
+      // action: appAction,
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: '/login',
+          element: <Login />,
+        },
+        {
+          path: '/register',
+          element: <Register />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    // loader: appLoader,
-    // action: appAction,
-    children: [
-      { index: true, element: <Home /> },
-      {
-        path: '/login',
-        element: <Login />,
-      },
-      {
-        path: '/register',
-        element: <Register />,
-      },
-    ],
+    basename: import.meta.env.PUBLIC_URL,
   },
-]);
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />,
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/zipplanet-frontend/', // https://<USERNAME>.github.io/<REPO>/ 형태로 배포하기위해
-  resolve: {
-    alias: [
-      { find: '@pages', replacement: '/src/pages' },
-      { find: '@components', replacement: '/src/components' },
-      { find: '@lib', replacement: '/src/lib' },
-      { find: '@assets', replacement: '/src/assets' },
-      { find: '@styles', replacement: '/src/styles' },
-    ],
-  },
-});
+export default ({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd()));
+  return defineConfig({
+    plugins: [react()],
+    base: process.env.VITE_BASENAME,
+    resolve: {
+      alias: [
+        { find: '@pages', replacement: '/src/pages' },
+        { find: '@components', replacement: '/src/components' },
+        { find: '@lib', replacement: '/src/lib' },
+        { find: '@assets', replacement: '/src/assets' },
+        { find: '@styles', replacement: '/src/styles' },
+      ],
+    },
+  });
+};
